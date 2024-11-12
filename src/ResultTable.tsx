08cd05686559dd4500363eb1eea11ab359236b77
Tamjid Rahman <tamjidarrahman@gmail.com>
2024-11-12 17:39:21 -0500

    update table

--- src/ResultTable.tsx
+++ src/ResultTable.tsx
@@ -11,9 +11,57 @@ const ResultTable: React.FC<ResultTableProps> = ({ result }) => {
   }
 
   return (
-    <div style={{ marginTop: "20px" }}>
+    <div
+      style={{
+        marginTop: "20px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
       <h2>SQL Result:</h2>
-      <div>{result}</div>
+      <div style={{ maxWidth: "100%", overflowX: "auto" }}>
+        <table
+          style={{
+            borderCollapse: "collapse",
+            tableLayout: "auto",
+            margin: "0 auto",
+          }}
+        >
+          <thead>
+            <tr>
+              {result[0] &&
+                Object.keys(result[0]).map((key) => (
+                  <th
+                    key={key}
+                    style={{
+                      border: "1px solid orange",
+                      padding: "8px",
+                      textAlign: "left",
+                      whiteSpace: "nowrap",
+                    }}
+                  >
+                    {key}
+                  </th>
+                ))}
+            </tr>
+          </thead>
+          <tbody>
+            {result.map((row, index) => (
+              <tr key={index}>
+                {Object.values(row).map((value, cellIndex) => (
+                  <td
+                    key={cellIndex}
+                    style={{ border: "1px solid orange", padding: "8px" }}
+                  >
+                    {value}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
